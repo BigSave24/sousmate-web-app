724579b7beca75f19d603626c1e40c1d4305b8e2
Kedmond24 <Kedmond24@live.com>
2020-10-15 17:12:08 -0400

    ADD randomGenerator function

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let testArray = [1,2,3,4,5,]
+let testNum = 10
+
+function randomGenerator(maxNum){
+    return Math.floor(Math.random() * Math.floor(maxNum));
+};
+
+
+console.log(randomGenerator(testNum));
