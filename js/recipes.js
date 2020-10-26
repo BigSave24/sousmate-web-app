b02a4413c8eeb81c20c43e6999bf9c7a28fe999c
Kedmond24 <Kedmond24@live.com>
2020-10-26 01:19:50 -0400

    UPDATE recipes.js with json object for receipes

---
+++ js/recipes.js
@@ -0,0 +1,117 @@
+let recipeList = [
+    {
+        'id': 01,
+        'name': 'Barbeque Chicken',
+        'category': 'Main Dish',
+        'synopsis': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Adipiscing commodo elit at imperdiet dui. Faucibus interdum posuere lorem ipsum dolor. Ullamcorper malesuada proin libero nunc consequat interdum varius.',
+        'instructions': [
+            'Preheat The Oven To 375 Degress.',
+            'In A Bowl Mix All Ingredients Instead Of Chicken.',
+            'Season The Chicken With Salt And Pepper.',
+            'Place The Chicken Breasts In A Baking Dish And Place In The Over. Cover.',
+            'Bake For 20 Minutes Or Until The Juices Run Clear.',
+            'Pour Over The Sauce Over The Chicken.',
+            'Bake For Another 5 Minutes Or Until Sauce Thickens Over The Chicken.',
+            'Remove From The Oven And Serve With Vegetables.'
+        ],
+        'prepTime': 0000,
+        'cookTime': 0000,
+        'yield': '3 Servings',
+        'level': '(Number)',
+        'ingredients': [
+            '2 Quarts Water',
+            '1 Cup Sugar',
+            '1 Tsp Salt',
+            '1/2 Cup Homemade BBQ Sauce',
+            '3 Chicken Breasts (Boneless, Skinless)',
+            '1 Glove Of Garlic',
+            '1/2 Tbsp Of Onion Powder',
+            '1 Chile (Red)'
+        ],
+        'author': 'Leroy Washington',
+        'imagePath': '(String)',
+    },
+    {
+        'id': 02,
+        'name': 'Simple Salad',
+        'category': 'Salad',
+        'synopsis': 'Tempus urna et pharetra pharetra massa massa ultricies mi. Lectus nulla at volutpat diam. Mi bibendum neque egestas congue. Non consectetur a erat nam at lectus urna duis. Velit ut tortor pretium viverra suspendisse potenti nullam ac tortor.',
+        'instructions': [
+            'On A Cutting Board, Slice The Radishes And Carrot',
+            'In A Large Bowl Mix All Ingredients.',
+            'Toss With A Large Spoon And Fork.',
+            'Serve Immediately Or Place In Refrigerator.'
+        ],
+        'prepTime': 0000,
+        'cookTime': 0000,
+        'yield': '1 Servings',
+        'level': '(Number)',
+        'ingredients': [
+            '1 Cup Of Lettuce',
+            '1 Cup Of Baby Spinach',
+            '1/2 Cup Of Cherry Tomatoes',
+            '1 Cucumber',
+            '2 Radishes',
+            '1 Carrot (Peeled)',
+            '2 Tbsp Of Your Favorite Dressing'
+        ],
+        'author': 'Alice Davis',
+        'imagePath': '(String)',
+    },
+    {
+        'id': 03,
+        'name': 'Chocolate Pudding',
+        'category': 'Dessert',
+        'synopsis': 'Et egestas quis ipsum suspendisse ultrices gravida dictum fusce. Scelerisque fermentum dui faucibus in ornare quam viverra. Cras fermentum odio eu feugiat pretium nibh ipsum consequat nisl. Amet nisl purus in mollis nunc sed.',
+        'instructions': [
+            'In A Large Saucepan, Stir Together Sugar, Cocoa, Cornstarch And Salt.',
+            'Put Saucepan On The Stove And Change The Setting To Medium-High Heat.',
+            'Bring The Mixture To A Boil And Cook Until Mixture Thickens, Stirring Constantly.',
+            'Remove Saucepan To A Cool Element And Stir In Butter And Vanilla Extract. Do NOT Melt Butter Beforehand.',
+            'Let Cool And Serve Warm.',
+            'Chill In Refrigerator After Done.'
+        ],
+        'prepTime': 0000,
+        'cookTime': 0000,
+        'yield': '8 Servings',
+        'level': '(Number)',
+        'ingredients': [
+            '1 Cup Of Sugar',
+            '1 Tsp Of Cinnamon',
+            '3 Tbsp Of Cocoa Chips, Unsweetened',
+            '1/4 Cup Of Cornstarch',
+            'Pinch Of Salt',
+            '3 Cups Of Milk',
+            '2 Tbsp Of Butter',
+            '1 Tsp Of Vanilla Extract'
+        ],
+        'author': '(String)',
+        'imagePath': '(String)',
+    }
+];
+
+
+
+// id (Number)
+// name (String)
+// category (String)
+// synopsis (String)
+// instructions (Array-String)
+// prepTime (Number)
+// cookTime (Number)
+// 'yield' (String)
+    // ** yield === serving size
+// level (Number)
+// ingredients (Array-String)
+// author (String)
+// imagePath (String)
+
+// Appetizer
+// Beverage
+// Soup
+// Salad
+// Vegetable
+// Main Dish
+// Bread
+// Dessert
+// Miscellaneous
