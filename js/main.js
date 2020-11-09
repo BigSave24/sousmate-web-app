1ff608ec654c18d7b0df9ef2d7a0d57dd331dae6
Kedmond24 <Kedmond24@live.com>
2020-11-09 11:22:45 -0500

    ADD functionality for hamburger menu button and side navigation menu

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-let testArray = [1,2,3,4,5,];
-let testNum = 10;
 let thumbnail = document.getElementById('recipe-thumbnail');
 let recipeBtn = document.getElementById('recipeBtn');
+let menuBtn = document.getElementById('menuBtn');
+let closeMenuBtn = document.getElementsByClassName('closeBtn');
+let sideMenu = document.getElementById('sideNav');
+let sideMenuLink = document.getElementsByClassName('side-menu-link');
 let recipeBookLength = Object.keys(recipeBook).length;
 let result;
 
-// function randomGenerator(maxNum){
-//     return Math.floor(Math.random() * Math.floor(maxNum));
-// };
+menuBtn.addEventListener('click', openSideMenu);
 
 function randomRecipe(maxNumRecipes){
     return Math.floor(Math.random() * Math.floor(maxNumRecipes));
@@ -22,7 +22,21 @@ function getRecipe(){
 
 };
 
-console.log(result = randomRecipe(recipeBookLength));
+function openSideMenu(){
+    sideMenu.style.width = '200px';
+    closeMenuBtn[0].addEventListener('click', closeSideMenu);
+
+    for (let i = 0; i < sideMenuLink.length; i++) {
+        sideMenuLink[i].addEventListener('click', closeSideMenu);
+    }
+    
+};
+
+function closeSideMenu(){
+    sideMenu.style.width = '0';
+};
+
+// console.log(result = randomRecipe(recipeBookLength));
 console.log(getRecipe());
 
 // recipeBtn.setAttribute('href', )
